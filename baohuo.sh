cc113be5bb8b1da0d98316b75574898665ff75b0
zjmkkk <157363626+zjmkkk@users.noreply.github.com>
2024-10-04 09:32:12 +0800

    Update baohuo.sh

--- baohuo.sh
+++ baohuo.sh
@@ -5,7 +5,7 @@ SERVERS_INFO="$1"
 TELEGRAM_BOT_TOKEN="$2"
 TELEGRAM_CHAT_ID="$3"
 
-# 验证JSON格式
+# 验证 JSON 格式
 if ! echo "$SERVERS_INFO" | jq . >/dev/null 2>&1; then
     echo "Error: Invalid JSON format in SERVERS_INFO"
     exit 1
@@ -19,11 +19,11 @@ failed_execs=""
 
 # 发送电报消息函数
 send_telegram_message() {
-    local message=$1
+    local message="$1"
+
     curl -s -X POST "https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage" \
         -d "chat_id=${TELEGRAM_CHAT_ID}" \
-        -d "text=${message}" \
-        -d "parse_mode=Markdown" >/dev/null 2>&1
+        --data-urlencode "text=${message}" >/dev/null 2>&1
 }
 
 # 从 JSON 字符串解析服务器信息为数组
@@ -37,42 +37,33 @@ fi
 
 # 循环遍历每个服务器信息
 while IFS= read -r server_info; do
-    # 使用 jq 解析 JSON
     ip=$(echo "$server_info" | jq -r '.ip')
     user=$(echo "$server_info" | jq -r '.user')
     pwd=$(echo "$server_info" | jq -r '.pwd')
     commands_array=$(echo "$server_info" | jq -r '.commands[]')
 
-    # 获取当前时间（北京时间）
     now=$(TZ='Asia/Shanghai' date +"%Y-%m-%d %H:%M:%S.%3N")
 
-    # 执行每个命令并检查结果
     while IFS= read -r cmd; do
-        # 直接使用命令而不转义
         sshpass -p "$pwd" ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -tt "$user@$ip" "$cmd" >/dev/null 2>&1
-
         if [ $? -eq 0 ]; then
-            message="${user} (${ip}) 于北京时间 ${now} 执行\"${cmd}\"成功！"
+            message="${user} (${ip}) 于北京时间 ${now} 执行命令成功：\"$cmd\""
             log+="${message}\n"
             ((success_count++))
         else
-            message="${user} (${ip}) 于北京时间 ${now} 执行\"${cmd}\"失败！"
+            message="${user} (${ip}) 于北京时间 ${now} 执行命令失败：\"$cmd\""
             log+="${message}\n"
             failed_execs+="${message}\n"
             ((failure_count++))
         fi
-
-        # 发送单个执行结果
         send_telegram_message "$message"
     done < <(echo "$commands_array")
 done < <(echo "$servers")
 
-# 总结统计信息
 summary="执行结果统计：
 成功执行的账号：${success_count}
 执行失败的账号：${failure_count}"
 
-# 如果有失败的执行，列出失败的账号
 if [ $failure_count -gt 0 ]; then
     summary+="
 
@@ -80,5 +71,4 @@ if [ $failure_count -gt 0 ]; then
 ${failed_execs}"
 fi
 
-# 发送汇总信息
 send_telegram_message "$summary"
